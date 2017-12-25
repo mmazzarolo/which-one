eb7e32a57a6a8df80699ed251020739943d7548a
mmazzarolo <mazzarolomatteo@gmail.com>
2017-12-25 22:44:04 +0100

    Improved font-rendering

--- src/index.js
+++ src/index.js
@@ -20,3 +20,28 @@ ReactDOM.render(
 );
 
 // registerServiceWorker();
+document.addEventListener('DOMContentLoaded', function(event) {
+  let isChrome = navigator.userAgent.indexOf('Chrome') > -1;
+  let isExplorer = navigator.userAgent.indexOf('MSIE') > -1;
+  let isFirefox = navigator.userAgent.indexOf('Firefox') > -1;
+  let isSafari = navigator.userAgent.indexOf('Safari') > -1;
+  let isOpera = navigator.userAgent.indexOf('Presto') > -1;
+  let isMac = navigator.userAgent.indexOf('Mac OS') != -1;
+  let isWindows = !isMac;
+
+  if (isChrome && isSafari) {
+    isSafari = false;
+  }
+
+  if (isSafari || isWindows) {
+    const body = document.getElementsByTagName('body')[0];
+    body.style['-webkit-text-stroke'] = '1.6px';
+    body.style['font-weight'] = '400';
+  }
+
+  if (isFirefox) {
+    const body = document.getElementsByTagName('body')[0];
+    body.style['-webkit-text-stroke'] = '1px';
+    body.style['font-weight'] = 'bold';
+  }
+});

731053e0546642ea1a3c037aca1436600be988cb
mmazzarolo <mazzarolomatteo@gmail.com>
2018-08-19 22:25:21 +0200

    Fixed config

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,10 +1,10 @@
 import { Screen } from "../types/Screen";
 
-const initialScreen: Screen = "PLAYGROUND";
+const initialScreen: Screen = "MENU";
 
 export default {
   INITIAL_SCREEN: initialScreen,
   ERROR_PENALITY_DELAY: 1000,
   INITIAL_ANIMATION_TIME: 1000,
-  TIME_LIMIT_IN_SECONDS: 200
+  TIME_LIMIT_IN_SECONDS: 20
 };
